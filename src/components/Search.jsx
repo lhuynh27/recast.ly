546aa05bdc3898fbb23bada307e4c23fda21fb3f
lhuynh27 <leannehuynh27@gmail.com>
2018-06-28 19:57:04 -0700

    Event firing

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,33 @@
-var Search = (props) => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      query: '',
+    }
+  }
+  
+  saveQuery () {
+    this.setState({
+      query: ''
+    })
+  }
+  
+  liveSearch (e) {
+    console.log(e.target.value)
+  }
+  
+  
+  render() {
+    return (
+    <div className="search-bar form-inline">
+      <input className="form-control" type="text" onChange={this.liveSearch} />
+      <button className="btn hidden-sm-down">
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div>
+    );
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
