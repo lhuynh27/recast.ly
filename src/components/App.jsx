84fa3e6b4d149a206f57655a1602e8169c93449a
lhuynh27 <leannehuynh27@gmail.com>
2018-06-28 17:21:59 -0700

    Pass videoPlayer and searchYouTube test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,35 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> fhdkjsahfkjdvnkal goes here</h5></div>
+class App extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      currentVideo: {}
+  }
+  
+  clickTitle(video){
+    this.setState({
+      currentVideo: video
+    });
+  } 
+  
+  render () {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search searchYouTube={this.props.searchYouTube} />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.props.data} clickTitle={this.clickTitle.bind(this)} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-      <VideoPlayer video={exampleVideoData[0]} />
-      </div>
-      <div className="col-md-5">
-        <VideoList videos={exampleVideoData}/>
-      </div>
-    </div>
-  </div>
-);
+    )}
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,23 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search",
+    data: {
+      q: options['query'],
+      maxResults: options['max'] ? options['max'] : 5,
+      part: 'snippet',
+      type: 'video',
+      videoEmbeddable: 'true',
+      key: options['key']
+    },
+    method: 'GET', 
+    success: function(data) {
+      callback(data.items);
+      console.log('WE GOT VIDEOS!');
+    },
+    error: function(){
+      console.error('YOU HAVE FAILED!');
+    }
+  })
 };
 
 window.searchYouTube = searchYouTube;
